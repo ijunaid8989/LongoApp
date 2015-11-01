8d581fae09aeea3486bda68140c23a794b9cd3de
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-11-01 18:13:16 +0500

    Added Mongolian instead of MongoDB again

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 var express = require('express')
   , app = express()
-  , mongo = require('mongodb');
+  , MongoClient = require('mongodb').MongoClient
+  , bodyParser = require('body-parser');
 
-var db = 
 
+app.use(bodyParser.json());
+
+var url = 'mongodb://localhost:27017/longo';
+
+var people;
+
+MongoClient.connect(url, function(err, db) {
+	people = db.collection('people');
+  	console.log("Connected correctly to server");
+});
+
+//var people = db.collection('people');
 
 
 
 app.set('view engine', 'ejs')
 
+
 app.get("/",function(req,res){
-	res.render("index");
+	res.render('index');
 });
 
- app.listen(3000);
+app.listen(3000);
